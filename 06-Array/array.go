305c7eeb48d1a6c4d9f80e079bff18da9677f2ba
Ayush Baral <ayushbaral010@gmail.com>
2023-08-08 03:52:19 +0530

    array

---
+++ 06-Array/array.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// A) One Type of Array Init
+	var fl [4]string 
+	fl[0] = "Apple"
+	fl[1] = "Banana"
+	fl[2] = "Cucumber"
+	fmt.Println(fl)
+	fmt.Println("Length of Array :", len(fl))
+
+	// B) Second Type of Array Init
+	var vl = [3]string{"potato","beans","mushroom"}
+	fmt.Println(vl)
+}
