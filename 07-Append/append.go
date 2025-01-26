e3005307b12edd08054becb3443be54733925af4
Ayush Baral <ayushbaral010@gmail.com>
2025-01-26 15:07:40 +0530

    append

--- 07-Append/append.go
+++ 07-Append/append.go
@@ -2,20 +2,25 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	var fruit = []string{"Apple", "Tomato", "Peach"}
+	dx := []string{"1", "2", "3"}
 	fruit = append(fruit, "Mango", "Banana")
 	fmt.Println(fruit)
 
 	// With Starting Point
 	fruit = append(fruit[1:])
 	fmt.Println(fruit)
-	
+
 	// With Ending Point
 	fruit = append(fruit[:3])
 	fmt.Println(fruit)
 
 	// With Both Starting and Ending Point
 	fruit = append(fruit[1:3])
+	fmt.Println(fruit)
+
+	//Append two array
+	fruit = append(fruit, dx...)
 	fmt.Println(fruit)
 }
